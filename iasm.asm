4bd591a164b6aab9b60b4cefb6d8425f1fee52cd
ForingBox <Shadowcorvo@hotmail.com>
2024-07-10 13:52:30 +0800

    Update iasm.asm

--- iasm.asm
+++ iasm.asm
@@ -1,101 +1,117 @@
 .model small
 .stack 100h
 .data
-
+    ; Messages to display when stock is low
     lowStockMsg1 db 10, 13, '*** ALERT: Biscuit stock is low! ***',  '$'
     lowStockMsg2 db 10, 13, '*** ALERT: Milk stock is low! ***',  '$'
     lowStockMsg3 db 10, 13, '*** ALERT: Candy stock is low! ***',  '$'
     lowStockMsg4 db 10, 13, '*** ALERT: Keyboard stock is low! ***',  '$'
     lowStockMsg5 db 10, 13, '*** ALERT: Microphone stock is low! ***', '$'
 
-
+    ; Item descriptions
     itemNum1 db 10,13, '1. Biscuit              ', 9, '$', 10
     itemNum2 db 10,13, '2. Milk                 ' , 9, '$', 10
     itemNum3 db 10,13, '3. Candy                ', 9, '$', 10
     itemNum4 db 10,13, '4. Keyboard             ', 9, '$', 10
     itemNum5 db 10,13, '5. Microphone           ', 9, '$', 10
 
+    ; Initial stock values
     itemN1Stock db 8     ; Stock for Biscuit
     itemN2Stock db 6     ; Stock for Milk
     itemN3Stock db 9     ; Stock for Candy    
-    itemN4Stock db 5     ; Stock for keyboard
+    itemN4Stock db 5     ; Stock for Keyboard
     itemN5Stock db 3     ; Stock for Microphone
 
-    mainMenu db 10,13, '=================================', 10,13
-             db '   Inventory System Main Menu', 10,13
-             db '=================================', 10,13
+    ; Main menu message
+    mainMenu db 10,13, '=======================================', 10,13
+             db '      Inventory System Main Menu', 10,13
+             db '=======================================', 10,13
              db '1. Create Order',10,13
              db '2. Check Inventory',10,13
              db '3. Exit',10,13 
-             db '=================================', 10,13, '$'
+             db '=======================================', 10,13, '$'
 
-    inventoryList db 10,13, '====================================', 10,13
-                 db '       Inventory Menu', 10,13
-                 db '====================================', 10,13
+    ; Inventory list header
+    inventoryList db 10,13, '=======================================', 10,13
+                 db '           Inventory Menu', 10,13
+                 db '=======================================', 10,13
                  db '   Item                     Quantity', 10,13, '$'
 
-
-    inventoryMenu db 10,13, '====================================', 10,13
+    ; Inventory menu options
+    inventoryMenu db 10,13, '=======================================', 10,13
                  db '1. Replenish Stock', 10,13
                  db '2. Reduce Stock', 10,13
                  db '3. Back', 10,13 
-                 db '====================================', 10,13,'$'
+                 db '=======================================', 10,13,'$'
 
-    orderList db 10,13, '====================================', 10,13
-                 db '           Order Menu',10,13
-                 db '====================================', 10,13
+    ; Order list header
+    orderList db 10,13, '=======================================', 10,13
+                 db '               Order Menu',10,13
+                 db '=======================================', 10,13
                  db '   Item                     Quantity', 10,13, '$'
 
-    orderMenu db 10,13,'====================================', 10,13
-                 db    'Select item (1-5)',10,13
+    ; Order menu options
+    orderMenu db 10,13,'=======================================', 10,13
+                 db 'Select item (1-5)',10,13
                  db '6. Back',10,13, '$'
-               
 
-    addStockmsg db 10,13,'====================================', 10,13
+    ; Restock menu message
+    addStockmsg db 10,13,'=======================================', 10,13
                 db 'Select the item you want to restock (1 - 5)',10,13
                 db '6. Back',10,13,'$'
     
-    decStockmsg db 10,13,'====================================', 10,13 
-                db  'Select the item you want to reduce (1 - 5)',10,13
-                db  '6. Back',10,13,'$'
+    ; Reduce stock menu message
+    decStockmsg db 10,13,'=======================================', 10,13 
+                db 'Select the item you want to reduce (1 - 5)',10,13
+                db '6. Back',10,13,'$'
     
+    ; Order prompt message
     orderMsg db 10,13, 'Select the item you want place order (1 - 5)',10,13,'$'
     
+    ; Input prompt
     choiceInput db 10,13, 'Enter your choice (1 - 3): $'
     
+    ; Invalid input message
     invalidInput db 10,13, 'Invalid Input, choose between 1 - 3',10,13,'$'
 
+    ; No stock decrease message
     noStockMsg db 10,13, 'Stock cannot be decreased. Item is out of stock.', 10,13, '$'
 
+    ; Full stock message
     fullStockMsg db 10,13, 'Stock cannot be increased. Item is in maximum stock.', 10,13, '$'
 
+    ; No stock order message
     noStockOrderMsg db 10,13, 'Stock cannot be order. Item is out of stock.', 10,13, '$'
 
+    ; No stock decrease message
     noStockDecMsg db 10,13, 'Stock cannot be decreased. Item is out of stock.', 10,13, '$'
 
+    ; Order created message
     orderCreatedMsg db 10,13, 'WE RICH THE ORDER HAS BEEN MADE.', 10,13,'$'
 
-
+; Macro to display a message
 ShowMessage Macro Mess
     mov ah, 09h
     mov dx, offset Mess
     int 21h
 EndM
 
+; Macro to get input from user
 GetInput Macro get
     mov ah, 01h
     int 21h
 EndM
 
-
+; Macro to decrease stock
 decStock Macro val
-	mov al,val
+    mov al,val
     cmp al, 0
     jle noStock
-	dec al
-	mov val, al
+    dec al
+    mov val, al
 EndM
 
+; Macro to order stock
 orderStock Macro val
     mov al,val
     cmp al, 0
@@ -104,57 +120,59 @@ orderStock Macro val
     mov val, al
 EndM
 
+; Macro to increase stock
 increStock Macro val
-	mov al,val
+    mov al,val
     cmp al,99
     jge fullStock
-	inc al
-	mov val, al
+    inc al
+    mov val, al
 EndM
 
+; Macro to display a digit
 ShowDigit Macro db 
     mov al,db
-	mov bl,0
-	add al,bl
-	
-	aam
-	add ax,3030h
-	
-	mov dh,al
-	mov dl,ah
-	
-	mov ah,2
-	int 21h
-	
-	mov dl,dh
-	mov ah,2
-	int 21h
+    mov bl,0
+    add al,bl
+    
+    aam
+    add ax,3030h
+    
+    mov dh,al
+    mov dl,ah
+    
+    mov ah,2
+    int 21h
+    
+    mov dl,dh
+    mov ah,2
+    int 21h
 EndM
 
+; Macro to blink a digit
 blink MACRO db
-    mov ah, 09h  	; 09h to print string
-    mov bl, 04h  	; 04h for color attribute
-    or bl, 80h   	; set bl bit 7 to 1 (blink attribute)
-    mov cx, 2    	; set cx to 2 (number of characters to print)
-    int 10h			; interrupt 10h (video services)
+    mov ah, 09h    ; 09h to print string
+    mov bl, 04h    ; 04h for color attribute
+    or bl, 80h     ; set bl bit 7 to 1 (blink attribute)
+    mov cx, 2      ; set cx to 2 (number of characters to print)
+    int 10h        ; interrupt 10h (video services)
 
-    mov al, db		; get value from variable db
+    mov al, db     ; get value from variable db
 
-	aam				; ASCII adjust ax after multiply
-    add ax, 3030h	; convert al to ASCII
+    aam            ; ASCII adjust ax after multiply
+    add ax, 3030h  ; convert al to ASCII
 
-	mov bl, 04h ; set color to red
+    mov bl, 04h    ; set color to red
 
-	; Split db into two registers
-    mov dh, al   	; dh --> carries lower nibble of AX(tens digit)
-    mov dl, ah	 	; dl --> carries upper nibble of AX(units digit)
+    ; Split db into two registers
+    mov dh, al     ; dh --> carries lower nibble of AX(tens digit)
+    mov dl, ah     ; dl --> carries upper nibble of AX(units digit)
 
-    mov ah, 2    	; 2 to print character
-    int 21h     	
-    mov dl, dh  	; move dh(lower digit) back to dl
-    int 21h     	
+    mov ah, 2      ; 2 to print character
+    int 21h        
+    mov dl, dh     ; move dh(lower digit) back to dl
+    int 21h        
 EndM
-
 
 .code
 MAIN PROC
